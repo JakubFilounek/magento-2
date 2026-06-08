b4333d4df37915bd947deb2395999b5ba1c84daa
Jakub Filounek <filounek@ecomail.cz>
2026-06-08 15:04:13 +0200

    Improve API key visibility toggle

--- app/code/Ecomail/Ecomail/view/adminhtml/web/js/unsaved-config-warning.js
+++ app/code/Ecomail/Ecomail/view/adminhtml/web/js/unsaved-config-warning.js
@@ -17,29 +17,57 @@ define([
                 return;
             }
 
+            function isMaskedPlaceholder() {
+                return /^\*+$/.test($field.val());
+            }
+
+            function setButtonState(isVisible) {
+                var label = isVisible ? $t('Hide API key') : $t('Show API key');
+
+                $button
+                    .toggleClass('is-visible', isVisible)
+                    .toggleClass('is-hidden', !isVisible)
+                    .attr({
+                        title: label,
+                        'aria-label': label
+                    });
+            }
+
             $field.data('ecomailApiKeyToggleReady', true);
             $field.addClass('ecomail-api-key-input');
             $field.wrap('<span class="ecomail-api-key-control"></span>');
 
             $button = $('<button/>', {
                 type: 'button',
-                class: 'action-default ecomail-api-key-toggle',
+                class: 'action-default ecomail-api-key-toggle is-hidden',
                 title: $t('Show API key'),
                 'aria-label': $t('Show API key')
-            }).html('<span aria-hidden="true">&#128065;</span>');
+            }).html('<span class="ecomail-eye-icon" aria-hidden="true"></span>');
 
             $field.after($button);
 
             $button.on('click', function () {
                 var isHidden = $field.attr('type') !== 'text';
-                var label = isHidden ? $t('Hide API key') : $t('Show API key');
 
+                if (isHidden && isMaskedPlaceholder()) {
+                    $button
+                        .attr({
+                            title: $t('Saved API key is hidden by Magento. Type a new API key to show it.'),
+                            'aria-label': $t('Saved API key is hidden by Magento. Type a new API key to show it.')
+                        })
+                        .addClass('is-masked');
+                    $field.trigger('focus');
+                    return;
+                }
+
+                $button.removeClass('is-masked');
                 $field.attr('type', isHidden ? 'text' : 'password');
-                $button.attr({
-                    title: label,
-                    'aria-label': label
-                });
+                setButtonState(isHidden);
                 $field.trigger('focus');
+            });
+
+            $field.on('input', function () {
+                $button.removeClass('is-masked');
             });
         }
 
